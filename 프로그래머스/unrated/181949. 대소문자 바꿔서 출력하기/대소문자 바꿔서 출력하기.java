e321c93f93d8e96bcd0a94e98cf86ab8a0597cc6
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-09-28 03:40:32 +0900

    Update 대소문자 바꿔서 출력하기.java

--- 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -19,3 +19,30 @@ public class Solution {
         System.out.println(result);
     }
 }
+
+
+/*
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String answer = "";
+
+        //Stack <Character> stack = new Stack <> ();
+
+        for(Character c : a.toCharArray()){
+            if(Character.isUpperCase(c)){
+                //stack.push(Character.toLowerCase(c));
+                answer += Character.toLowerCase(c);
+            }
+            else if(Character.isLowerCase(c)){
+                //stack.push(Character.toUpperCase(c));
+                answer += Character.toUpperCase(c);
+            }
+        } 
+        System.out.println(answer);
+    }
+}
+*/
+
+
