46c550d1586e6df3f0a60d9b5f9a847e0dd49700
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-09-28 03:39:09 +0900

    Update 문자열 반복해서 출력하기.java

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -10,3 +10,21 @@ public class Solution {
         System.out.println(result);
     }
 }
+
+
+/*
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        String answer = "";
+        for (int i = 1; i <= n; i++) {
+            answer += str;
+        }
+        System.out.println(answer);
+
+    }
+}
+*/
