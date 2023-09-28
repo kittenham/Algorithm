67581847742ca1ae6418597c6c59690ce38c25a5
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-09-29 01:16:37 +0900

    Update 문자열 섞기.java - 내가 생각한 다른풀이, 다른사람의 풀이 추가

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,13 +1,46 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        int length = str1.length();
 
-        for(int i = 0; i<length; i++){
+        //두 문자열의 길이가 같은 것이 제한사항이기 때문에 str1을 그냥 사용했음.
+        for(int i = 0; i<str1.length(); i++){ 
             answer+=str1.charAt(i);
             answer+=str2.charAt(i);
         }
-
         return answer;
     }
 }
+
+/*
+<내가 생각한 다른 풀이>
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+
+        //두 문자열의 길이가 같은 것이 제한사항이기 때문에 str1을 그냥 사용했음.
+        for(int i = 0; i<str1.length(); i++){ 
+            result.append(str1.charAt(i));
+            result.append(str2.charAt(i));
+        }
+        return answer;
+    }
+}
+*/
+
+/*
+<다른사람의 풀이>
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+
+        int length = str1.length();
+        String[] str1arr = str1.split("");
+        String[] str2arr = str2.split("");
+        for (int i=0; i<length; i++) {
+            answer += str1arr[i]+str2arr[i];
+        }
+        return answer;
+    }
+}
+
+*/
