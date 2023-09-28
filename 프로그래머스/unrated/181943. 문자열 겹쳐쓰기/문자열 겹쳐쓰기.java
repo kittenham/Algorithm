7419b2c180a3fad8fe99869610b60a48e65f4cc4
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-09-28 21:46:43 +0900

    Update 문자열 겹쳐쓰기.java
    다른풀이

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -5,7 +5,22 @@ class Solution {
         //인덱스 
         int index = s+overwrite_string.length();
 
+        //answer는 0부터 s전까지 그리고 s부터 overwrite_string의 문자로 index만큼 바꾼다.
         answer = my_string.substring(0,s)+overwrite_string+my_string.substring(index);
         return answer;
     }
 }
+
+
+/*다른풀이
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        
+        return before + overwrite_string + after;
+    }
+}
+*/
+
+
