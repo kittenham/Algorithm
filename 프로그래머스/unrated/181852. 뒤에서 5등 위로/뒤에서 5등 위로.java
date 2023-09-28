8db309649c1401bbbd8cd302e2cff05af3b5d76b
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-09-29 01:06:55 +0900

    Update 뒤에서 5등 위로.java - 오래걸린 이유, 다른사람 풀이 추가

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -14,3 +14,30 @@ class Solution {
         return result;
     }
 }
+
+/*
+아 솔직히 너무 어려웠음
+시간이 오래걸렸던 이유 => import java.util.Arrays; 얘를 안넣어서 Arrays가 적용이 안되었다.
+*/
+
+/*
+다른사람 풀이1)
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numList) {
+        return Arrays.stream(numList).sorted().skip(5).toArray();
+    }
+}
+
+다른사람 풀이2)
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        Arrays.sort(num_list);
+        answer = Arrays.copyOfRange(num_list,5,num_list.length);
+        return answer;
+    }
+}
+*/
