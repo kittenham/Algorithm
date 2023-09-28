e12aad94b23d56610794033b22bce0b05a557222
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-09-28 21:19:23 +0900

    Update 문자열 돌리기.java

--- 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -9,3 +9,14 @@ public class Solution {
         }
     }
 }
+
+/* 다른풀이
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for (char ch : a.toCharArray())
+            System.out.println(ch);
+    }
+}
+*/
