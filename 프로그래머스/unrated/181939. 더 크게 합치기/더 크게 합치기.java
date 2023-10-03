cf2612469be486ca6ec141549eb1f36993559b35
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-04 00:25:33 +0900

    Update 더 크게 합치기.java - 주석추가, 다른사람 풀이 추가

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int a, int b) {
-
+        //정수를 문자열로 변환
         String aStr = Integer.toString(a);
         String bStr = Integer.toString(b);
 
-        String cStr = aStr+bStr;
-        String dStr = bStr+aStr;
+        String cStr = aStr+bStr;  //a ⊕ b
+        String dStr = bStr+aStr;  //b ⊕ a
 
+        //다시 문자열을 정수로 변환
         int result1 = Integer.parseInt(cStr);
         int result2 = Integer.parseInt(dStr);
 
@@ -15,6 +16,22 @@ class Solution {
         } else{
             return result2;
         }
-
     }
 }
+
+
+/*다른사람 풀이*/
+/*
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int aLong = Integer.parseInt(""+a+b);
+        int bLong = Integer.parseInt(""+b+a);
+        answer = aLong > bLong ? aLong : bLong;
+
+        return answer;
+    }
+}
+*/
+
+
