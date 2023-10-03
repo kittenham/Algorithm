14273ed40f80465fef71720bde8432fc16f1e211
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-04 00:10:03 +0900

    Update 문자열 곱하기.java - 나의 다른 풀이, 다른사람 풀이 추가

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -7,3 +7,30 @@ class Solution {
         return answer;
     }
 }
+
+
+/*나의 다른 풀이*/
+/*
+class Solution {
+    public String solution(String my_string, int k) {
+        StringBuilder answer = new StringBuilder();
+        for(int i=0; i<k; i++){
+            answer.append(my_string);
+        }
+        return answer.toString();
+    }
+}
+*/
+
+
+
+/*다른사람 풀이*/
+/*
+class Solution {
+    public String solution(String my_string, int k) {
+        return my_string.repeat(k);
+    }
+}
+*/
+
+
