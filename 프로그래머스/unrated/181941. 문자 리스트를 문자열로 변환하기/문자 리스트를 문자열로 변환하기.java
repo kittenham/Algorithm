51787a3f34550bd05d36137b33cba7bd960f9911
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-03 23:58:22 +0900

    Update 문자 리스트를 문자열로 변환하기.java - 내 다른풀이, 다른사람풀이추가

--- 프로그래머스/unrated/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
+++ 프로그래머스/unrated/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
@@ -7,3 +7,27 @@ class Solution {
         return answer;
     }
 }
+
+
+/*나의 다른풀이*/
+/*
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        for(int i = 0; i < arr.length; i++){
+            answer += arr[i];
+        }
+        return answer;
+    }
+}
+*/
+
+
+/*다른사람 풀이*/
+/*
+class Solution {
+    public String solution(String[] arr) {
+        return String.join("", arr);
+    }
+}
+*/
