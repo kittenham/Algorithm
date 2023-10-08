6fe0db596be533adb816f3af2f09a8f06c239383
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-08 23:46:44 +0900

    Update 공배수.java - 다른풀이 추가

--- 프로그래머스/unrated/181936. 공배수/공배수.java
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -9,3 +9,16 @@ class Solution {
         return answer;
     }
 }
+
+
+/*나의 다른 풀이*/
+/* => 삼항연산 사용
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        return number%n==0 && number%m==0 ? 1 : 0;
+    }
+}
+*/
+
+
