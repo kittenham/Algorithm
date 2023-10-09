18d1ca007369492ac320bc9946b3e57e3f9633ff
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-10 00:47:04 +0900

    Update 이어 붙인 수.java - 주석달기, 다른풀이 추가

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -5,6 +5,7 @@ class Solution {
 
         for(int num : num_list){
             if(num%2==0){
+               //이어붙인수가 되기 때문에 짝수합에서*10을 하여 일의 자리를 비게 한 후 그 자리에 num 숫자를 집어넣으면 이어붙인 것처럼 숫자가 나온다.
                 even = even*10 + num;
             } else{
                 odd = odd*10 + num;
@@ -13,3 +14,24 @@ class Solution {
         return odd+even;
     }
 }
+
+
+//다른풀이
+/*문자열로 숫자들을 이어붙인 후 다시 숫자로 변환하기*/
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String odd = "", even = ""; //문자열로 변환
+
+        for ( int i = 0; i < num_list.length; i++ ) {
+            if ( num_list[i] % 2 == 0 ) {
+                even += num_list[i];
+            } else odd += num_list[i];
+        }
+
+        answer = Integer.parseInt(odd) + Integer.parseInt(even);  //숫자로 변환
+
+        return answer;
+    }
+}
+
