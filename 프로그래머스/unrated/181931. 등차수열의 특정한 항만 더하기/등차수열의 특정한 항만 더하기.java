d6aa4dd31f7cac2aec0bba286137fad8a17e8207
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-10 00:31:57 +0900

    Update 등차수열의 특정한 항만 더하기.java - 주석 추가, 다른사람 풀이 추가
    다른사람 풀이 - Stream 사용

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -2,10 +2,23 @@ class Solution {
     public int solution(int a, int d, boolean[] included) {
         int answer = 0;
         for(int i=0; i<included.length; i++){
-            if(included[i]){
-                answer += a+i*d;
+            if(included[i]){ 
+                //항을 선택하는 경우 해당 값을 더함
+                answer += a+(i*d);
             }
         }
         return answer;
     }
 }
+
+
+/*다른사람 풀이*/
+/*
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        return IntStream.range(0, included.length).map(idx -> included[idx]?a+(idx*d):0).sum();
+    }
+}
+*/
