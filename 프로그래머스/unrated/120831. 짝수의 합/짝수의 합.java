f46fc15ccc1abec3eb8df39fa47003753514478c
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-11 23:51:55 +0900

    Update 짝수의 합.java - 다른사람 풀이 추가

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -9,3 +9,29 @@ class Solution {
         return answer;
     }
 }
+
+
+/*다른사람 풀이-1*/
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i=2; i<=n; i+=2){ //2부터 시작해서 i를 +2를 해서 짝수만 나오도록해서 더해주도록함
+            answer+=i;
+        }
+
+        return answer;
+    }
+}
+
+
+/*다른사람 풀이-2*/
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        return IntStream.rangeClosed(0, n)
+            .filter(e -> e % 2 == 0)
+            .sum();
+    }
+}
