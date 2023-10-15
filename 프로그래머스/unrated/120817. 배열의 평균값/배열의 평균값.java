e4e1b2003ad73c5a23e1c8fb7cee203dfd2cf76b
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-15 21:05:56 +0900

    Update 배열의 평균값.java - 다른사람 풀이 추가

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
@@ -9,3 +9,21 @@ class Solution {
         return answer;
     }
 }
+
+
+/*다른사람 풀이 - Stream으로 풀기*/
+import java.util.Arrays;
+
+class Solution {
+    public double solution(int[] numbers) {
+        return Arrays.stream(numbers).average().orElse(0);
+    }
+}
+
+/*
+.orElse 설명
+.orElse(0) -> 주어진 값이 null이 아닌 경우 주어진 값을 되돌려주고 null인 경우 인수(0)을 돌려준다.
+*/
+
+
+
