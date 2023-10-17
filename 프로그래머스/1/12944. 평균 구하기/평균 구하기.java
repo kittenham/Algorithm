f7d218690a2fede89f6630292f5934afe32b3b4a
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-17 17:31:46 +0900

    Update 평균 구하기.java - 다른풀이 추가

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -7,3 +7,17 @@ class Solution {
         return answer;
     }
 }
+
+//다른풀이
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum =0;
+        for(int i : arr){
+           sum += i;
+        }
+        answer = (double)sum/arr.length;
+        return answer;
+    }
+}
+
