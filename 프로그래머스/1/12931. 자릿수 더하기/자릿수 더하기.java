bf4fd855ac58c42a44e8c546e281c33a16f0b286
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-18 23:59:59 +0900

    Update 자릿수 더하기.java - 다른사람풀이 추가

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -11,3 +11,19 @@ public class Solution {
         return answer;
     }
 }
+
+
+//다른사람풀이
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String[] array = String.valueOf(n).split("");
+        for(String s : array){
+            answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
+
