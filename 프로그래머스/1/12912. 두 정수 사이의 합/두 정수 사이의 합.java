cdf141f2bfd501d1b8ada4c35e35d51a0a7678a6
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-10-19 23:53:25 +0900

    Update 두 정수 사이의 합.java - 다른사람 풀이 2개 추가

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -12,3 +12,30 @@ class Solution {
         return answer;
     }
 }
+
+
+//다른사람 풀이 -1
+class Solution {
+  public long solution(int a, int b) {
+      long answer = 0;
+      for (int i = ((a < b) ? a : b); i <= ((a < b) ? b : a); i++) 
+          answer += i;
+
+      return answer;
+  }
+}
+
+
+//다른사람 풀이 -2 => 등자수열 합 공식((a1+an)/2 * n)으로 구하기
+class Solution {
+
+    public long solution(int a, int b) {
+        return sumAtoB(Math.min(a, b), Math.max(b, a));
+    }
+
+    private long sumAtoB(long a, long b) {
+        return (b - a + 1) * (a + b) / 2;
+    }
+}
+
+
