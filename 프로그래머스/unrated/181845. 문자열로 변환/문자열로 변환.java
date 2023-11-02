f5d117e9a78797fd9cce084c4aa0cb0b0d49abb7
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-11-02 14:40:26 +0900

    Update 문자열로 변환.java - 다른풀이 추가, 속도 처리 이유

--- 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -3,3 +3,15 @@ class Solution {
         return Integer.toString(n);
     }
 }
+
+//다른풀이
+class Solution {
+    public String solution(int n) {
+        return n+"";
+    }
+}
+
+/*
+첫번째 방법이 두번째 방법보다 처리속도가 훨씬 빠르다. 쉽기는 두번째가 더 쉽긴한데..ㅋㅋㅋ
+첫번째방법은 0.02ms 정도 걸린다면 두번째 방법은 7.02ms 정도 걸린다.
+*/
