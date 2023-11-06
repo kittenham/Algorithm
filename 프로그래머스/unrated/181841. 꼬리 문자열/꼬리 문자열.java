b7418017819c858092f55dc40731a4a28e3381b5
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-11-06 11:20:36 +0900

    Update 꼬리 문자열.java - 다른풀이 추가

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
@@ -9,3 +9,17 @@ class Solution {
         return answer;
     }
 }
+
+
+//다른풀이
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        String answer = "";
+        for(int i=0; i<str_list.length; i++){
+            if(!str_list[i].contains(ex)){
+                answer += str_list[i];
+            }
+        }
+        return answer;
+    }
+}
