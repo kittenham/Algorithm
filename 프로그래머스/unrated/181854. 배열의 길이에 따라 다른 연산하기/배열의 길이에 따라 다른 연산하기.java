d7798315b5f6cd6817c8f017379e38bbdc1eec19
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-11-06 14:08:18 +0900

    Update 배열의 길이에 따라 다른 연산하기.java - 다른사람 풀이 추가
    for문에서 초기화식 부분을 삼항연산자로 조건을 주고 연산했음.

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.java
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.java
@@ -11,3 +11,15 @@ class Solution {
         return answer;
     }
 }
+
+
+//다른사람풀이
+class Solution {
+    public int[] solution(int[] arr, int n) {
+        for(int idx=arr.length%2==0?1:0; idx<arr.length; idx+=2) {
+            arr[idx]+=n;
+        }
+        return arr;
+    }
+}
+
