3fae0fe60891bcd64050f4f43d53ce0bca4e0f2c
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-11-07 12:50:26 +0900

    Update 문자열 뒤집기.java - 다른풀이 추가

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -4,3 +4,18 @@ class Solution {
         return reversed.reverse().toString();
     }
 }
+
+
+//나의 다른풀이 추가
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+
+        for(int i=my_string.length()-1; i>=0; i--){
+            answer+=my_string.charAt(i);
+        }
+
+        return answer;
+    }
+}
+
