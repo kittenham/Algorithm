a9f09b65f255469019e54508233b94ebaa856501
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-11-08 19:53:58 +0900

    Update 0 떼기.java - 다른풀이 추가

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -5,3 +5,23 @@ class Solution {
         return answer;
     }
 }
+
+
+//다른풀이
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        int i = 0;
+        while (i < n_str.length() && n_str.charAt(i) == '0') {
+            i++;
+        }
+        
+        while (i < n_str.length()) {
+            answer += n_str.charAt(i);
+            i++;
+        }
+        return answer;
+    }
+}
+
