afb4678e8cccaaf88371081a4e7109551f78c8a2
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-11-11 21:28:47 +0900

    Update 짝수 홀수 개수.java - 다른사람풀이 추가

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -11,3 +11,18 @@ class Solution {
         return answer;
     }
 }
+
+
+//다른사람 풀이 추가
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        for(int i = 0; i < num_list.length; i++)
+            answer[num_list[i] % 2]++;
+               //숫자를 2로 나누면 0과 1만 나타나게 되고 answer배열의 자릿수도 2개이니 answer[0], answer[1] 밖에 되지 않아 식이 성립이 가능하다.
+
+        return answer;
+    }
+}
+
