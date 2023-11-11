0c8a292dc09eb6127b626b92c9e524edb9fd10bd
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-11-11 21:43:01 +0900

    Update 배열의 유사도.java - 다른풀이 추가

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.java
@@ -1,3 +1,4 @@
+//1. Set을 이용해 중복값 없애고 유사도 측정하기
 import java.util.HashSet;
 
 class Solution {
@@ -21,3 +22,24 @@ class Solution {
         return set1.size();
     }
 }
+
+
+//2. for 반복문 사용. + equals() 메소드를사용함.
+//equals() 메소드가 아닌 ==을 사용하면 원하는 값이 출력되지 않음.
+//이유는 문자열을 비교할때는 equals() 메소드를 사용해야 하기 때문이다. 
+//==연산자는 두 문자열의 참조를 비교하는 것이 아니라 메모리 주소를 비교한다.
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for(int i=0; i<s1.length; i++){
+            for(int j=0; j<s2.length; j++){
+                if(s1[i].equals(s2[j])){
+                    answer ++;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
