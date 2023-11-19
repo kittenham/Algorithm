2ff008b44b58b96efd0d73e8f9ccb0057afa0f23
Yeji Kim <129197025+kittenham@users.noreply.github.com>
2023-11-19 14:42:55 +0900

    [level unrated] Title: 대문자와 소문자, Time: 12.05 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,7 +1,17 @@
-import java.util.stream.Collectors;
-
 class Solution {
-    public String solution(String myString) {
-        return myString.chars().mapToObj(operand -> String.valueOf((char) (Character.isLowerCase(operand) ? Character.toUpperCase(operand) : Character.toLowerCase(operand)))).collect(Collectors.joining());
+    public String solution(String s) {
+        String answer = "";
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>=97 && s.charAt(i)<=122){
+                answer += (char)(s.charAt(i)-32);
+            } else if(s.charAt(i)>=65 && s.charAt(i)<=90){
+                answer += (char)(s.charAt(i)+32);
+            } else {
+                answer += s.charAt(i);
+            }
+        }
+
+        return answer;
     }
 }
